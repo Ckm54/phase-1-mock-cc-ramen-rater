3e4f0ef2c464fda8910bffb4521ad3ab33b631b1
Collins <mutaic84@gmail.com>
2022-04-18 00:46:49 +0300

    display menu items on page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const menuContainer = document.getElementById("ramen-menu")
+
+    fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(element => {
+            loadMenu(element)
+        });
+    })
+
+    function loadMenu(item) {
+        let image = document.createElement("img")
+        image.setAttribute("src", `${item.image}`)
+        menuContainer.append(image)
+
+        image.addEventListener("click", () => {
+            displayInfo(item)
+        })
+    }
+
+    
+})
